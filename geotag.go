a41a5908361c4af327748f9e514f78643b803e2d
sfomuseumbot <sfomuseumbot@localhost>
2023-03-10 12:00:07 -0800

    update to use aaronland/go-http-static; update aaronland/go-http-leaflet; add support for including JS resources at end of HTML

--- geotag.go
+++ geotag.go
@@ -2,14 +2,11 @@ package geotag
 
 import (
 	"fmt"
-	"github.com/aaronland/go-http-leaflet"
-	"github.com/aaronland/go-http-rewrite"
-	"github.com/sfomuseum/go-http-leaflet-geotag/static"
-	"io/fs"
-	_ "log"
 	"net/http"
-	"path/filepath"
-	"strings"
+
+	"github.com/aaronland/go-http-leaflet"
+	aa_static "github.com/aaronland/go-http-static"
+	"github.com/sfomuseum/go-http-leaflet-geotag/static"
 )
 
 var INCLUDE_LEAFLET = true
@@ -17,6 +14,9 @@ var INCLUDE_LEAFLET = true
 type LeafletGeotagOptions struct {
 	JS  []string
 	CSS []string
+	// AppendJavaScriptAtEOF is a boolean flag to append JavaScript markup at the end of an HTML document
+	// rather than in the <head> HTML element. Default is false
+	AppendJavaScriptAtEOF bool
 }
 
 func DefaultLeafletGeotagOptions() *LeafletGeotagOptions {
@@ -37,11 +37,6 @@ func DefaultLeafletGeotagOptions() *LeafletGeotagOptions {
 
 func AppendResourcesHandler(next http.Handler, opts *LeafletGeotagOptions) http.Handler {
 
-	if INCLUDE_LEAFLET {
-		leaflet_opts := leaflet.DefaultLeafletOptions()
-		next = leaflet.AppendResourcesHandler(next, leaflet_opts)
-	}
-
 	return AppendResourcesHandlerWithPrefix(next, opts, "")
 }
 
@@ -49,58 +44,16 @@ func AppendResourcesHandlerWithPrefix(next http.Handler, opts *LeafletGeotagOpti
 
 	if INCLUDE_LEAFLET {
 		leaflet_opts := leaflet.DefaultLeafletOptions()
+		leaflet_opts.AppendJavaScriptAtEOF = opts.AppendJavaScriptAtEOF
 		next = leaflet.AppendResourcesHandlerWithPrefix(next, leaflet_opts, prefix)
 	}
 
-	js := opts.JS
-	css := opts.CSS
+	static_opts := aa_static.DefaultResourcesOptions()
+	static_opts.CSS = opts.CSS
+	static_opts.JS = opts.JS
+	static_opts.AppendJavaScriptAtEOF = opts.AppendJavaScriptAtEOF
 
-	if prefix != "" {
-
-		for i, path := range js {
-			js[i] = appendPrefix(prefix, path)
-		}
-
-		for i, path := range css {
-			css[i] = appendPrefix(prefix, path)
-		}
-	}
-
-	ext_opts := &rewrite.AppendResourcesOptions{
-		JavaScript:  js,
-		Stylesheets: css,
-	}
-
-	return rewrite.AppendResourcesHandler(next, ext_opts)
-}
-
-func AssetsHandler() (http.Handler, error) {
-
-	http_fs := http.FS(static.FS)
-	return http.FileServer(http_fs), nil
-}
-
-func AssetsHandlerWithPrefix(prefix string) (http.Handler, error) {
-
-	fs_handler, err := AssetsHandler()
-
-	if err != nil {
-		return nil, err
-	}
-
-	prefix = strings.TrimRight(prefix, "/")
-
-	if prefix == "" {
-		return fs_handler, nil
-	}
-
-	rewrite_func := func(req *http.Request) (*http.Request, error) {
-		req.URL.Path = strings.Replace(req.URL.Path, prefix, "", 1)
-		return req, nil
-	}
-
-	rewrite_handler := rewrite.RewriteRequestHandler(fs_handler, rewrite_func)
-	return rewrite_handler, nil
+	return aa_static.AppendResourcesHandlerWithPrefix(next, static_opts, prefix)
 }
 
 func AppendAssetHandlers(mux *http.ServeMux) error {
@@ -114,51 +67,9 @@ func AppendAssetHandlersWithPrefix(mux *http.ServeMux, prefix string) error {
 		err := leaflet.AppendAssetHandlersWithPrefix(mux, prefix)
 
 		if err != nil {
-			return err
+			return fmt.Errorf("Failed to append Leaflet assets, %w", err)
 		}
 	}
 
-	asset_handler, err := AssetsHandlerWithPrefix(prefix)
-
-	if err != nil {
-		return nil
-	}
-
-	walk_func := func(path string, info fs.DirEntry, err error) error {
-
-		if path == "." {
-			return nil
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		if prefix != "" {
-			path = appendPrefix(prefix, path)
-		}
-
-		if !strings.HasPrefix(path, "/") {
-			path = fmt.Sprintf("/%s", path)
-		}
-
-		// log.Println("APPEND", path)
-
-		mux.Handle(path, asset_handler)
-		return nil
-	}
-
-	return fs.WalkDir(static.FS, ".", walk_func)
-}
-
-func appendPrefix(prefix string, path string) string {
-
-	prefix = strings.TrimRight(prefix, "/")
-
-	if prefix != "" {
-		path = strings.TrimLeft(path, "/")
-		path = filepath.Join(prefix, path)
-	}
-
-	return path
+	return aa_static.AppendStaticAssetHandlersWithPrefix(mux, static.FS, prefix)
 }
